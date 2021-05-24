0aa5dad74fb1802eff120536b6b5cd6426551c45
Aschale <aschaled@gmail.com>
2021-05-24 00:50:23 -0500

    Added css styling and 5 services rendering properly

--- server/index.js
+++ server/index.js
@@ -1,50 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-
-const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/api/book/:id', (req, res) => {
-  axios.get(`http://localhost:2002/api/book/${id}`)
-  .then((response) => {
-    res.status(202).json(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-});
-
-app.get('/api/price/:id', (req, res) => {
-  axios.get(`http://localhost:3000/api/price/${id}`)
-  .then((response) => {
-    res.status(202).json(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-});
-
-// app.get('/api/price/:bookTitle', (req, res) => {
-//   axios.get(`http://localhost:3000/api/price/${bookTitle}`)
-//   .then((response) => {
-//     res.status(202).json(response.data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+// app.get('/api/audible/:bookId', (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, './public/index.html'));
 // });
 
-
-app.get('/api/summary/:id', async (req, res) => {
-  axios.get(`http://localhost:1220/api/summary/${id}`)
+app.get('/api/book/:bookId', (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:2002'})
+  axios.get(`http://localhost:2002${req.url}`)
   .then((response) => {
     res.status(202).json(response.data);
   })
@@ -53,8 +25,20 @@ app.get('/api/summary/:id', async (req, res) => {
   });
 });
 
-app.get('/reviews/:id', (req, res) => {
-  axios.get(`http://localhost:4000/reviews/${id}`)
+app.get('/api/price/:bookId', (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:3000'})
+  axios.get(`http://localhost:3000${req.url}`)
+  .then((response) => {
+    res.status(202).json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+app.get('/api/price/:bookTitle', (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:3000'})
+  axios.get(`http://localhost:3000${req.url}`)
   .then((response) => {
     res.status(202).json(response.data);
   })
@@ -64,6 +48,45 @@ app.get('/reviews/:id', (req, res) => {
 });
 
 
+app.get('/api/summary/:bookId', async (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:1220'})
+  axios.get(`http://localhost:1220${req.url}`)
+  .then((response) => {
+    res.status(202).json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+
+app.get('/api/aggReview/:bookId', async (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:2880'})
+  axios.get(`http://localhost:2880${req.url}`)
+  .then((response) => {
+    res.status(202).json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+app.get('/reviews/:bookId', (req, res) => {
+  res.set({'Access-Control-Allow-Origin': 'http://localhost:4000'})
+  axios.get(`http://localhost:4000${req.url}`)
+  .then((response) => {
+    res.status(202).json(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+let port = 3300;
+
+// if(!module.parent){
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
+
